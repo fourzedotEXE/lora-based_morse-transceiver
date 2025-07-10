83f02801383c953d0f71dd92a22e2d07a57248aa
JACK <62486003+fourzedotEXE@users.noreply.github.com>
2025-07-09 20:30:21 -0400

    Update main_device.ino

--- main_device.ino
+++ main_device.ino
@@ -9,12 +9,17 @@
                   Main Device (Hook up to PC)  
 */                                             
 #include <SPI.h>
-#include <LoRa.h>
 #include <AESLib.h>
+#include <SoftwareSerial.h>
 
-#define LORA_FREQ 915E6     // SX1278 frequency
 #define SERIAL_BAUD 9600    // Serial baud rate
 #define INPUT_BUFFER_LIMIT 128 // designed for Arduino UNO, not stress-tested anymore (this works with readBuffer[129])
+
+SoftwareSerial lora(11, 10);  //RX, TX for RYLR896
+
+//Randomly generated addresses
+const int DEST_ADDR = 47575;  // Change this to the address of the remote node
+const int SELF_ADDR = 27597;  // This device's address
 
 unsigned char cleartext[INPUT_BUFFER_LIMIT] = {0}; // THIS IS INPUT BUFFER (FOR TEXT)
 unsigned char ciphertext[2*INPUT_BUFFER_LIMIT] = {0}; // THIS IS OUTPUT BUFFER (FOR BASE64-ENCODED ENCRYPTED DATA)
@@ -49,26 +54,26 @@ uint16_t decrypt_to_cleartext(byte msg[], uint16_t msgLen, byte iv[]) {
 }
 
 void setup() {
+  //set baud
   Serial.begin(SERIAL_BAUD);
   Serial.setTimeout(60000);
   delay(2000);
 
+  //set lora serial baud
+  lora.begin(115200);
+
   while (!Serial);
 
   //Initialize LoRa
-  Serial.println("LoRa Two-Way Chat");
+  lora.println("AT+ADDRESS=" + String(SELF_ADDR));
+  delay(100);
+  lora.println("AT+NETWORKID=5");
+  delay(100);
+  lora.println("AT+BAND=915000000");
+  delay(100);
   
-  if (!LoRa.begin(LORA_FREQ)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
+  Serial.println("LoRa initialized");
 
-  // Optional tuning
-  LoRa.setSpreadingFactor(12);
-  LoRa.setSignalBandwidth(125E3);
-  LoRa.setCodingRate4(5);
-
-  Serial.println("LoRa Init OK");
   aes_init(); // generate random IV, should be called only once? causes crash if repeated...
 
   Serial.println("Type a message and press ENTER to send.");
@@ -94,7 +99,7 @@ byte enc_iv_from[N_BLOCK] = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0x
 uint16_t encLen = 0;
 
 //encrypt the contents of readBuffer using the shared key and iv
-byte encryption_layer(unsigned char* readBuffer, int len){
+String encryption_layer(unsigned char* readBuffer, int len){
   Serial.print("readBuffer length: "); Serial.println(sizeof(readBuffer));
   Serial.print("readBuffer contents: "); Serial.println((char*)readBuffer);
 
@@ -111,16 +116,36 @@ byte encryption_layer(unsigned char* readBuffer, int len){
   Serial.print("Encrypted contents: ");
   Serial.println((char*)ciphertext);
 
+  String hex_input = byteArrayToHex(readBuffer, len);
+
+  Serial.print("Hex encrypted contents: ");
+  Serial.println(hex_input);
+
   //free memory
   Serial.println("---");
   memset(readBuffer, 0, len+1);
   free(readBuffer);
   readBuffer = NULL;  // Optional but good practice to avoid dangling pointer
+
+  return hex_input;
 }
 
 //decrypt the contents of readBuffer using the shared key and iv
-void decryption_layer(unsigned char* readBuffer, int len){
-  //decrypt
+String decryption_layer(String hex_data){
+  //decode byte data from hex
+  int len = hex_data.length();
+  int out_len = 0;
+  if (len%2 == 0){
+    out_len = 0;
+    return ""; 
+  }
+  //1 byte == 2 hex chars
+  out_len = len/2;
+  //allocate appropriate amount of data for byte string
+  byte* readBuffer = (byte*)malloc(out_len);
+  readBuffer = hexToByteArray(hex_data, readBuffer, out_len);
+
+  //decrypt ciphertext
   Serial.println("Encrypted. Decrypting..."); Serial.println(encLen ); Serial.flush();
   
   unsigned char base64decoded[50] = {0};
@@ -131,57 +156,87 @@ void decryption_layer(unsigned char* readBuffer, int len){
   Serial.print("Decrypted cleartext of length: "); Serial.println(decLen);
   Serial.print("Decrypted cleartext:\n"); Serial.println((char*)cleartext);
 
-  //compare decrypt integrity
-  if (strcmp((char*)readBuffer, (char*)cleartext) == 0) {
-    Serial.println("Decrypted correctly.");
-  } else {
-    Serial.println("Decryption test failed.");
-  }
-
-  //free memory
+  //free hex decoded bytes
   Serial.println("---");
-  memset(readBuffer, 0, len+1);
+  memcpy(readBuffer, 0, out_len+1);   //MIGHT NEED TO DELETE IF PROGRAM CRASHES
   free(readBuffer);
-  readBuffer = NULL;  // Optional but good practice to avoid dangling pointer
+  readBuffer = NULL;
 }
 
-String inputText = "";
 
-void loop() {
-  // Check if a message was received
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
-    }
-    Serial.println();
+String byteArrayToHex(const byte* data, int len) {
+  String hex = "";
+  for (int i = 0; i < len; i++) {
+    if (data[i] < 16) hex += "0";
+    hex += String(data[i], HEX);
+  }
+  return hex;
+}
+
+byte* hexToByteArray(String hex_data, byte* hex_decode, int out_len){
+  //loop over each 2 hex chars
+  for (int i = 0; i < out_len; i++) {
+    String byte_as_string = hex_data.substring((2*i), (2*i+2));  //ex: i = 1  -> hex_data.substring(2, 4) = 0xA4
+
+    //to convert to byte, use .c_str(), and then convert that to an unsigned long
+      //strtoul is only compatible with c-style strings
+    //then cast the long to a byte and add that to the proper index in the array
+    hex_decode[i] = (byte)strtoul(byte_as_string.c_str(), nullptr, 16);  //add the newly decoded byte to the array
   }
 
-  // Check if user typed something
-  while (Serial.available()) {
-    char c = Serial.read();
-    if (c == '\n') {
-      if (inputText.length() > 0) {
-        wait(1);
-        Serial.println(inputText);
+  return hex_decode;
+}
 
-        //copy string message data over to the encryption buffer
-        int len = inputText.length();
-        unsigned char* readBuffer = (unsigned char*)malloc(len+1);
-        if (readBuffer != NULL) {
-          strcpy((char*)readBuffer, inputText.c_str());  // Copy string content
-        }
-        encryption_layer(readBuffer, len);
+String input = "";
+String message = "Ahab: ";
+String received = "Peqd: ";
+String incoming = " ";
 
-        //call lora write here (write the ciphertext, not the readBuffer)
-        LoRa.beginPacket();
-        LoRa.write((byte*)ciphertext, sizeof(ciphertext));
-        LoRa.endPacket();
+void loop() {
 
-        inputText = "";
+  // Received message from LoRa
+  if (lora.available()) {
+    incoming = lora.readStringUntil('\n');
+
+    //decode the data
+    String decoded_payload = decryption_layer(incoming);
+
+    //assume code here that translates message to audible morse(not in scope of proj.)
+
+    Serial.println(decoded_payload);
+  }
+
+  //--------------------------------------------------------
+
+  // Sending encrypted morse data LoRa
+  if (Serial.available()) {
+    Serial.println("message generating...");
+    String inputText = Serial.readStringUntil('\n');
+    input.trim();
+
+    
+    if (inputText.length() > 0) {
+      wait(1);
+      Serial.println(inputText);
+
+      //copy string message data over to the encryption buffer
+      int len = inputText.length();
+      unsigned char* readBuffer = (unsigned char*)malloc(len+1);
+      if (readBuffer != NULL) {
+        strcpy((char*)readBuffer, inputText.c_str());  // Copy string content
       }
-    } else {
-      inputText += c;
+
+      //NOTE THAT THE ENCRPYTION LAYER ALSO PERFORMS A HEX ENCODE ON THE DATA
+      String final_payload = encryption_layer(readBuffer, len);
+
+      //call lora commands here (write the ciphertext, not the readBuffer)
+      String command = "AT+SEND=" + String(DEST_ADDR) + "," + String(final_payload.length()) + "," + final_payload;
+      lora.println(command);
+      Serial.println("Sent: " + final_payload);
+
+      //decryption_layer(readBuffer, len);
+
+      inputText = "";
     }
   }
 }
